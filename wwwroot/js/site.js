5b942a369858b92a2854d1fdff7b1d1f87055bff
Will <doggy.huang@gmail.com>
2020-08-15 22:37:20 +0800

    Add SignalR JavaScript client sample code (ChatHub)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    alert(`Hi ${user}, you said: ${message}`)
+});
+
+connection.start().then(function () {
+    connection.invoke("SendMessage", 'Will', 'Hello World').catch(function (err) {
+        return console.error(err.toString());
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
